401f6d7d47f5c45afdbba3ce61098399dc38b489
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-07 17:54:34 -0300

    criação de tabela com prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,12 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'hello'
+app.post('/polls', (request) => {
+    console.log(request.body)
+    return 'hello nlw'
 })
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
+
