b6fdbe91b3aeb57b2fdb93f99de428e156905fa2
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-09 16:40:21 -0300

    regras de negócio para votos na enquete

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -19,8 +19,31 @@ export async function voteOnPoll(app: FastifyInstance){
 
         let { sessionId } = request.cookies
 
+        if(sessionId){
+            const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+                where:{
+                    sessionId_pollId:{
+                        sessionId,
+                        pollId 
+                    }
+                }
+            })
+
+            if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId){
+                await prisma.vote.delete({
+                    where:{
+                        id: userPreviousVoteOnPoll.id
+                    }
+                })
+
+            }else if(userPreviousVoteOnPoll){
+                return reply.status(400).send({message: 'Your already voted on this poll.'})
+            }
+
+        }
+
         if(!sessionId){
-            const sessionId = randomUUID()
+            sessionId = randomUUID()
             
             reply.setCookie('sessionId', sessionId, {
                 path: '/',
@@ -30,6 +53,14 @@ export async function voteOnPoll(app: FastifyInstance){
             })
         }
 
-        return reply.status(201).send({sessionId})
+        await prisma.vote.create({
+            data:{
+                sessionId,
+                pollId,
+                pollOptionId
+            }
+        })
+
+        return reply.status(201).send()
     })
 }
