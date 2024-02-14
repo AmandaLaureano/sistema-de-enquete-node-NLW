ce0522aa9c015d26d4fb7deacb1f7f4a9267fafa
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-14 17:31:16 -0300

    Funcionalidade de ranking das opções mais votadas com REDIS

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
 import fastifyCookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { pollResults } from '../ws/poll-results'
 
 const app = fastify()
 
@@ -12,9 +14,12 @@ app.register(fastifyCookie, {
     parseOptions: {}
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server running!")
